b9c3b5f9c870cb8bcde656c094d77a161f352123
laharipriyabudida57-gif <laharipriyabudida57@gmail.com>
2025-10-30 06:27:20 +0530

    Add files via upload

---
+++ 5 numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e,f;
+	printf("enter a&b&c&d&e value");
+	scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+	f=a+b+c+d+e;
+	printf("sum of five numbers=%d",&f);
+	return 0;
+}
