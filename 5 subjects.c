75a45f14e5e19950bf75bfeb72ee43e349928e11
laharipriyabudida57-gif <laharipriyabudida57@gmail.com>
2025-10-30 06:27:42 +0530

    Add files via upload

---
+++ 5 subjects.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e,total;
+	float avg;
+	printf("enter a&b&c&d&e values");
+	scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+	total=a+b+c+d+e;
+	avg=total/5;
+	printf("avg=%f",avg);
+	printf("total=%d",total);
+	return 0;
+}
