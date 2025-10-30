4b85f25c5f3b5204db8f799d859aa5d6b00ec63c
laharipriyabudida57-gif <laharipriyabudida57@gmail.com>
2025-10-30 06:30:03 +0530

    Add files via upload

---
+++ avg of 2 numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum,avg;
+	printf("enter a,b values");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	avg=sum/2;
+	printf("avg of two integers=%d",avg);
+	return 0;
+}
